ef65c3450cb0f9fceabbcb945414aa297551adc8
bencreizy <bencreizy@gmail.com>
2025-10-22 15:10:47 -0400

    Refactor index.tsx for improved error handling

--- index.tsx
+++ index.tsx
@@ -1,15 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+// Mount the React app to the root div
+const rootElement = document.getElementById("root") as HTMLElement;
 
-const rootElement = document.getElementById('root');
 if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
+  throw new Error("Root element not found. Make sure index.html has <div id='root'>");
 }
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
